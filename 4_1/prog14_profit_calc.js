3c935e089c7613ab0ba1887d4337c419bc5814d5
Cyrano <cyrano@cyranowebdev.com>
2020-07-13 19:10:30 -0300

    Adicionando exercício 10 de cálculo de lucro nas vendas.

---
+++ 4_1/prog14_profit_calc.js
@@ -0,0 +1,10 @@
+let pCost = 40;
+let pPrice = 120;
+
+if (pCost > 0 || pPrice > 0) {
+  let profit = (pPrice - (pCost * 1.2)) * 1000;
+  console.log('O lucro da empresa com a venda de mil produtos será de',profit,'.');
+} else {
+  console.log('Erro: valores menores que zero não são aceitos.');
+}
+  
