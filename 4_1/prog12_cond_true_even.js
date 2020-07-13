cb7c05682a1654a1a1793378692d256691b12453
Cyrano <cyrano@cyranowebdev.com>
2020-07-13 19:03:39 -0300

    Adicionando exercícios 8 e 9 de ifs para par ou ímpar.

---
+++ 4_1/prog12_cond_true_even.js
@@ -0,0 +1,12 @@
+let num1 = 80;
+let num2 = 21;
+let num3 = 3;
+let result;
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+  result = true;
+} else {
+  result = false;
+}
+console.log(result);
+  
