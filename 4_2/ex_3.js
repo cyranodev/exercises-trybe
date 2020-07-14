bc0f2de455223304f47fc3a32b29a36648cb6cd3
Cyrano <cyrano@cyranowebdev.com>
2020-07-14 18:27:51 -0300

    corrigindo prints ex3 e add exercicio 4 - 4_2

--- 4_2/ex_3.js
+++ 4_2/ex_3.js
@@ -2,8 +2,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbersTotal = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
-  console.log(i);
-  console.log(numbers[i]);
   numbersTotal = numbersTotal + numbers[i];
 }
 
