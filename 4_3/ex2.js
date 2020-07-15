5343dc6da1740fb726923a52da7fef0ff46632fd
Cyrano <cyrano@cyranowebdev.com>
2020-07-15 18:01:52 -0300

    Melhorando codigo do ex.1 e adicionando exercicio 2 - 4_3.

--- 4_3/ex2.js
+++ 4_3/ex2.js
@@ -3,11 +3,9 @@ let nColunas = '';
 let nLinhas = 0;
 
 if (n > 1) {
-  for (i = 1; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {
     nColunas += '*';
-  }
-
-  for (iLinhas = 1; iLinhas <= n; iLinhas++) {
     console.log(nColunas);
   }
 }
+
