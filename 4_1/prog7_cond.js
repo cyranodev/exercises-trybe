bc89752ae6b2d57cd77a4f5e03896999bc1cb624
Cyrano <cyrano@cyranowebdev.com>
2020-07-16 18:00:45 -0300

    apagando arquivos com nomes incorretos - exercícios 4_1.

--- 4_1/prog7_cond.js
+++
@@ -1,5 +0,0 @@
-let a = 1;
-let b = 7;
-let c = 187064;
-
-let maiorNum = (a > b && a > c) ? console.log(a) : (b > a && b > c) ? console.log(b) : console.log(c);
--- 4_1/prog8_cond.js
+++
@@ -1,3 +0,0 @@
-let num = 1;
-
-let sinalDoNum = (num > 0) ? console.log(num,'is positive') : (num < 0) ? console.log(num,'is negative') : console.log(num,'is zero');
