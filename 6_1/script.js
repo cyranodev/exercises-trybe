af8456ab84106b6b9610f240ee308bc53e203afe
cyranowebdev <cyrano@cyranowebdev.com>
2020-08-03 13:52:25 -0300

    avançando nos exercícios, parte I do form.

--- 6_1/script.js
+++ 6_1/script.js
@@ -1,11 +1,48 @@
-let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO',];
+let estados = {
+  AC: 'Acre',
+  AL: 'Alagoas',
+  AM: 'Amazonas',
+  AP: 'Amapá',
+  BA: 'Bahia',
+  CE: 'Ceará',
+  DF: 'Distrito Federal',
+  ES: 'Espírito Santo',
+  GO: 'Goiás',
+  MA: 'Maranhão',
+  MG: 'Minas Gerais',
+  MS: 'Mato Grosso do Sul',
+  MT: 'Mato Grosso',
+  PA: 'Pará',
+  PB: 'Paraíba',
+  PE: 'Pernambuco',
+  PI: 'Piauí',
+  PR: 'Paraná',
+  RJ: 'Rio de Janeiro',
+  RN: 'Rio Grande do Norte',
+  RO: 'Rondônia',
+  RR: 'Roraima',
+  RS: 'Rio Grande do Sul',
+  SC: 'Santa Catarina',
+  SE: 'Sergipe',
+  SP: 'São Paulo',
+  TO: 'Tocantins',
+};
 
 let selectEstado = document.querySelector('#estado');
 
-for (let i = 0; i < estados.length; i += 1) {
+for (const property in estados) {
   let option = document.createElement('option');
-  option.value = estados[i];
-  option.id = estados[i];
-  option.innerHTML = estados[i];
+  option.value = `${property}`;
+  option.id = `${property}`;
+  option.innerHTML = `${estados[property]}`;
   selectEstado.appendChild(option);
 }
+
+function checkMail(event) {
+  const emailText = event.target.value;
+  const regEx = RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/, 'i');
+  console.log(regEx.test(emailText));
+}
+
+let emailInput = document.querySelector('#email');
+emailInput.addEventListener('keyup', checkMail);
