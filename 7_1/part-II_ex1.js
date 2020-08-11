7f92347ee6d07234c31d9e4323574bd0ab4ebbef
cyranowebdev <cyrano@cyranowebdev.com>
2020-08-11 11:33:01 -0300

    add bônus do exercicio 1 - parte II - 7_1.

--- 7_1/part-II_ex1.js
+++ 7_1/part-II_ex1.js
@@ -1,14 +1,3 @@
-const factorial = number => number > 1 ? number * factorial(number - 1) : 1
-  
+const factorial = number => number > 1 ? number * factorial( number - 1 ) : 1
 
-
-const factorial = number => 
-number > 1 ?
-number * factorial(
-  number > 1 ? number * factorial(
-    number => number > 1 ? number * factorial(
-      number => number > 1 ? number * factorial(  
-) : 1
-
-
-console.log(factorial(5))
+console.log(factorial(5));
