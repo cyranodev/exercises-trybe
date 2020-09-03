e53feda3539bcf84d9cf763c2b77d02745e29c3f
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-03 15:28:52 -0300

    Refat - ex04 - add.

---
+++ 7_3/Refat_04.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+function secondThirdSmallest(array) {
+  array.sort((x, y) => x - y);
+  const results = [ array[1], array[2] ];
+  return results;
+};
+
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
+
+assert.strictEqual(typeof secondThirdSmallest(parameter), 'object');
+assert.deepEqual(secondThirdSmallest(parameter), result);
