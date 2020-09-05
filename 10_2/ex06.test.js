1480e6649cb7fd0fe5f1e34a15103f67f7e82d54
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-05 16:17:07 -0300

    10_2 - Ex06 - add 6.2

--- 10_2/ex06.test.js
+++ 10_2/ex06.test.js
@@ -20,14 +20,33 @@ const findAnimalByName = (name) => (
 const getAnimal = (name) => {
   return  findAnimalByName(name);
 };
+
+const findAnimalByAge = (age) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const animalArray = Animals.filter(animal => animal.age === age);
+      if (animalArray.length > 0) {
+        return resolve(animalArray);
+      }
+
+      return reject({ error: 'Nenhum animal com essa idade!' });
+    }, 100);
+  })
+);
+
+const getAge = (age) => {
+  return  findAnimalByAge(age);
+};
+
+
 // ---------------------
 
 describe('Testando promise - findAnimalByName', () => {
   describe('Quando existe o animal com o nome procurado', () => {
     test('Retorne o objeto do animal', () => {
       expect.assertions(1);
-      return getAnimal('Dorminhoco').then(animal => {
-        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      return getAnimal('Dorminhoco').then((array) => {
+        expect(array).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
       });
     });
   });
@@ -37,6 +56,26 @@ describe('Testando promise - findAnimalByName', () => {
       expect.assertions(1);
       return getAnimal('Bob').catch(error =>
         expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
+
+describe('Testando promise - findAnimalByAge', () => {
+  describe('Quando existe o animal com a idade procurada', () => {
+    it('Retorne um array com o(s) animal(is)', () => {
+      expect.assertions(1);
+      return getAge(2).then(animal => {
+        expect(animal).toEqual([{ name: 'Soneca', age: 2, type: 'Dog' }]);
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com a idade procurada', () => {
+    it('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAge(3).catch(error =>
+        expect(error).toEqual({ error: 'Nenhum animal com essa idade!' })
       );
     });
   });
