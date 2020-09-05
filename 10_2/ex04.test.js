cc4837e80feb2401c5558c59f886a869fd0cd619
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-05 15:29:33 -0300

    10_2 - ex04 - correcting jest package.

--- 10_2/ex04.test.js
+++ 10_2/ex04.test.js
@@ -1,4 +1,4 @@
-const fetch = require('../node-fetch');
+const fetch = require('node-fetch');
 
 const getRepos = (url) => {
   return fetch(url)
@@ -8,9 +8,11 @@ const getRepos = (url) => {
     });
 }
 
-describe('Function getRepos should return a list of repos', async () => {
-  const url = 'https://api.github.com/users/tryber/repos';
-  const list = await getRepos(url);
-  expect(list).toContain('sd-01-week4-5-project-todo-list');
-  expect(list).toContain('sd-01-week4-5-project-meme-generator');
+describe('Function getRepos - promises', () => {
+  it('should return a list that contains certain names', async () => {
+    const url = 'https://api.github.com/users/tryber/repos';
+    const list = await getRepos(url);
+    expect(list).toContain('sd-01-week4-5-project-todo-list');
+    expect(list).toContain('sd-01-week4-5-project-meme-generator');
+  });
 });
