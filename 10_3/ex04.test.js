11affc72dec2ef23d148f293ac295aa36af8b856
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-08 15:19:26 -0300

    10_3 - ex04 - iniciando arquivos.

---
+++ 10_3/ex04.test.js
@@ -0,0 +1,8 @@
+// Dentro de um mesmo arquivo, crie três funções.
+// A primeira deve receber uma string e retorná-la em caixa alta.
+// A segunda deve também receber uma string e retornar só a primeira letra.
+// A terceira deve receber duas strings e concatená-las.
+// Faça o mock do arquivo:
+// A primeira função deve retornar a string em caixa baixa.
+// A segunda função deve retornar a última letra de uma string.
+// A terceira deve receber três strings e concatená-las.
